d978657b854d6585b55a72b7ad7c3c1917d13561
Nicole <143754076+NicoleNageli@users.noreply.github.com>
2025-01-19 15:36:27 -0700

    working task fields

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # EisenhowerMatrix
 to-do list side by side with eisenhower matrix that sorts tasks into 4 quadrants
+
+## Features
+- when task is marked completed in matrix, cross & gray out task in to do list
--- app.js
+++ app.js
@@ -1,14 +1,49 @@
-document.addEventListener(
-    'DOMContentLoaded', 
-    () => {
-  let cells = 
-      document.querySelectorAll('td[data-value]');
-  cells.forEach(function (cell) {
-      let input = 
-          document.createElement('input');
-      input.type = 'text';
-      input.value = cell.dataset.value;
-      cell.innerHTML = '';
-      cell.appendChild(input);
-  });
-});
+//store tasks in array
+let tasks = [];
+//check if all inputs filled in properly before adding task
+function checkInputs()
+{
+    //get info from table input
+    const taskName = document.getElementById("taskName").value;
+    const urgency = document.getElementById("urgencyDropdown").value;
+    const importance = document.getElementById("importanceDropdown").value;
+    const difficulty = document.getElementById("difficultyDropdown").value;
+    const dueDate = document.getElementById("dueDate").value;
+
+    //check if all inputs filled
+    //&& is for multiple conditions
+    //check each field separately
+    if(urgency !== 'selectOption' && importance !== 'selectOption' && difficulty !== 'selectOption' && taskName && dueDate )
+    {
+        addTask(taskName, urgency, importance, difficulty, dueDate);
+    }
+    else
+    {
+        alert("Please fill in all fields to add a task.")
+    }
+
+}
+
+//add new task to array and table
+function addTask(taskName, urgency, importance, difficulty,dueDate,daysUntil)
+{
+    
+    //calculate days until due using due date
+    //add to table
+    document.getElementById("daysUntilCell").textContent = daysUntilDue(dueDate);
+    //push instead of append bc more than 1+ new value to be added
+    tasks.push({taskName, urgency, importance, difficulty,dueDate,daysUntil});
+    //console.log("Task Name: ", taskName, urgency, importance, difficulty,dueDate,daysUntil);
+}
+
+//function takes in todays date and task due date, calculates days til due
+function daysUntilDue(dueDate)
+{
+    const today = Date.now(); //Date in milliseconds
+    const due = new Date(dueDate); //convert dueDate to Date object
+    //difference in milliseconds
+    const differenceMill = due - today;
+    //convert milliseconds to days, rounds up to whole number
+    const daysUntil = Math.ceil(differenceMill/(1000 * 60 * 60 * 24));
+    return daysUntil;
+}
